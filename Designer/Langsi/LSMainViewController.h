05957568bcb9506b279ca479b0bd81a93b75a21a
zhzenghui <zhzenghui@gmail.com>
2014-08-07 12:29:47 +0800

    add ls main view

---
+++ Designer/Langsi/LSMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  LSMainViewController.h
+//  Designer
+//
+//  Created by bejoy on 14/8/7.
+//  Copyright (c) 2014年 zeng hui. All rights reserved.
+//
+
+#import "LSViewController.h"
+
+@interface LSMainViewController : LSViewController
+
+@end
---
+++ Designer/Langsi/LSMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  LSMainViewController.m
+//  Designer
+//
+//  Created by bejoy on 14/8/7.
+//  Copyright (c) 2014年 zeng hui. All rights reserved.
+//
+
+#import "LSMainViewController.h"
+
+@interface LSMainViewController ()
+
+@end
+
+@implementation LSMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
